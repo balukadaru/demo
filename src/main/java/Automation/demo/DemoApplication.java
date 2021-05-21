c09609ed25a918023e1e28c988684a6a1b2b24c6
balukadaru <balukadaru@gmail.com>
2021-05-21 14:28:40 +0530

    This is my 4th Commit

--- src/main/java/Automation/demo/DemoApplication.java
+++ src/main/java/Automation/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.print("HihiHi");
+		System.out.print("HihiHiHi");
 		SpringApplication.run(DemoApplication.class, args);
 		
 	}
